3ab4e15b8a58bbd07f0778d83b9612708b17722e
silver-lu <silver.zlu@gmail.com>
2014-11-10 13:40:34 -0800

    Added Unit test that test to make sure server is up Have to use Threads...

--- src/main/java/com/ericsmith/Server.java
+++ src/main/java/com/ericsmith/Server.java
@@ -1,9 +1,46 @@
 package com.ericsmith;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-    public static void main(String[] args)
-    {
+    public static String SERVER_START_COMMAND;
+    private int portNumber;
 
+    public Server(int portNumber) {
+        this.portNumber = portNumber;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server(5000);
+        server.run();
+
+    }
+
+    public int getPort() {
+        return portNumber;
+    }
+
+    public void run() throws IOException {
+        ServerSocket listener = new ServerSocket(this.portNumber);
+        try {
+            Socket clientSocket = listener.accept();
+            try {
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                out.println("Server Up");
+            }
+            finally {
+                clientSocket.close();
+            }
+
+        }
+        finally {
+            listener.close();
+        }
     }
 }
--- src/test/java/com/ericsmith/ServerTest.java
+++ src/test/java/com/ericsmith/ServerTest.java
@@ -1,4 +1,46 @@
 package com.ericsmith;
 
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.*;
+
 public class ServerTest {
+
+    Thread serverThread;
+
+    @Test
+    public void testServerCanBeInitializedWithPortNumber() throws Exception {
+        Server server = new Server(5000);
+        assertEquals(5000, server.getPort());
+    }
+
+    @Test
+    public void testServerCanBeStarted() throws Exception {
+        startPrivateServer(5000);
+        Socket serverSocket = new Socket("localhost", 5000);
+        BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+        assertEquals("Server Up", in.readLine());
+    }
+
+    private void startPrivateServer(final int portNumber) {
+        serverThread = new Thread() {
+            public void run() {
+                Server server = new Server(portNumber);
+                try {
+                    server.run();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        serverThread.start();
+    }
 }
