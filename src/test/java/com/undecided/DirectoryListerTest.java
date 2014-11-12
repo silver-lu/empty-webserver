6f1664eb97a824204efe1e7716d336f16714829f
Yvonne Wang <Yvonne.Wang@ticketmaster.com>
2014-11-12 10:45:44 -0800

    commit DirectoryListTest for testing hidden file and directory

--- src/test/java/com/undecided/DirectoryListerTest.java
+++ src/test/java/com/undecided/DirectoryListerTest.java
@@ -6,7 +6,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.*;
 
 /**
  * Created by silver.lu on 11/10/14.
@@ -36,21 +38,46 @@ public class DirectoryListerTest {
         MockFile mockFile = new MockFile(".");
 
         List<File> fakeFiles = new ArrayList<File>();
-        fakeFiles.add(new MockFile("./abc"));
-
+        MockFile normalFile = new MockFile("./abc");
+        normalFile.setIsFile(true);
+        fakeFiles.add(normalFile);
 
         MockFile hiddenFile = new MockFile("./HiddenFile");
+        hiddenFile.setIsFile(true);
         hiddenFile.flagAsHidden();
         fakeFiles.add(hiddenFile);
 
         mockFile.setFiles(fakeFiles);
 
         DirectoryLister directoryLister = new DirectoryLister(mockFile);
-        System.out.println(directoryLister.getReadableFiles().toString());
+        assertTrue(pathExists(directoryLister.getReadableFiles(), "./abc"));
+        assertFalse(pathExists(directoryLister.getReadableFiles(), "./HiddenFile"));
+
     }
 
     @Test
     public void testHiddenDirectoriesAreNotPulledUp() throws Exception {
+
+        MockFile mockFile = new MockFile(".");
+        List<File> fakeDirs = new ArrayList<File>();
+
+        MockFile dir1 = new MockFile("DIR1");
+        dir1.setIsDirectory(true);
+
+        fakeDirs.add(dir1);
+
+
+        MockFile hiddenDir = new MockFile("HIDDENDIR");
+        hiddenDir.setIsDirectory(true);
+        hiddenDir.flagAsHidden();
+        fakeDirs.add(hiddenDir);
+
+
+        mockFile.setFiles(fakeDirs);
+
+        DirectoryLister directoryLister = new DirectoryLister(mockFile);
+        assertTrue(pathExists(directoryLister.getReadableDirectories(), "DIR1"));
+        assertFalse(pathExists(directoryLister.getReadableDirectories(), "HIDDENDIR"));
 
     }
 
